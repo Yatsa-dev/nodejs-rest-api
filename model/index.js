03368d5d8f313d01bee48dc70e5ea43cce808e05
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2022-01-04 18:58:05 +0200

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,20 +1,39 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises');
+const path = require('path');
+const crypto = require('crypto');
 
-const listContacts = async () => {}
+const readContent = async () => {
+  const content = await fs.readFile(
+    path.join(__dirname, 'db', 'contacts.json'),
+    'utf8'
+  );
+  const result = JSON.parse(content);
+  return result;
+};
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  return await readContent();
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await readContent();
+  const [contact] = contacts.filter((contact) => contact.id === contactId);
+  return contact;
+};
 
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+function removeContact(contactId) {
+  // ...твой код
 }
+
+const addContact = async (name, email, phone) => {
+  const contacts = await readContent();
+  const newContact = { name, email, phone, id: crypto.randomUUID() };
+  contacts.push(newContact);
+  await fs.writeFile(
+    path.join(__dirname, 'db', 'contacts.json'),
+    JSON.stringify(contacts, null, 2)
+  );
+  return newContact;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
